bb950c06b05e2fdcfceae18a3526576ed287dc22
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-22 19:05:02 +0600

    up

--- src/Pages/Shared/LeftSideNav/LeftSIdeNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSIdeNav.js
@@ -5,7 +5,7 @@ const LeftSIdeNav = () => {
 const [categories, setCategories] = useState([]);
 
 useEffect(() => {
-   fetch('http://localhost:5000/news-categories')
+   fetch('https://dragon-news-server-mo-60-node.vercel.app/news-categories')
    .then(res => res.json())
    .then(data => setCategories(data));
 }, []);
